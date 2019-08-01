923d1f094cfd566c068deee31d86c5123c148edf
Nir Shalmon <nir.shalmon00@gmail.com>
2019-08-02 00:47:49 +0300

    dynamically update like count on frontend

--- static/post_list_script.js
+++ static/post_list_script.js
@@ -1,5 +1,8 @@
+var like_counts = new Map()
+
 function on_like_unlike(){
     var post_id = $(this).closest('div').attr('id');
+    var like_count = like_counts.get(post_id);
     if($(this).text() == 'like'){
         $.post("/like/",
         {
@@ -8,18 +11,27 @@ function on_like_unlike(){
         }
         );
         $(this).text('unlike');
+        like_count += 1;
      }else{
         $.post("/unlike/",
         {
             post_id: post_id,
             csrfmiddlewaretoken: token,
         }
-        );
-        $(this).text('like');
-     }
+       );
+       $(this).text('like');
+       like_count -= 1;
+    }
+    like_counts.set(post_id,like_count);
+    $(this).siblings(".like_count").each(set_like_count);
     return false;
+}
+
+function set_like_count(){
+    $(this).text(like_counts.get($(this).closest('div').attr('id')));
 }
 
 $(document).ready(function(){
   $(".like_unlike_button").click(on_like_unlike);
+  $(".like_count").each(set_like_count);
 });
