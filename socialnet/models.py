8200373eb22ce0898bf2beb7937694d9d4a5e3aa
Nir Shalmon <nir.shalmon00@gmail.com>
2019-08-13 15:06:31 +0300

    fix likes

--- socialnet/models.py
+++ socialnet/models.py
@@ -1,10 +1,17 @@
 import datetime
 
 from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.db import models
 from django.utils import timezone
+
+
+class LikeableMixin(models.Model):
+    likes = models.ManyToManyField(get_user_model(),
+                                   related_name="%(app_label)s_%(class)s_like_set",
+                                   related_query_name="%(app_label)s_%(class)s_likes")
+
+    class Meta:
+        abstract = True
 
 
 class PublishedText(models.Model):
@@ -14,17 +21,14 @@ class PublishedText(models.Model):
     )
     text = models.CharField(max_length=50000, verbose_name="Post Text Contents")
     pub_time = models.DateTimeField('Publication time', default=timezone.now)
-    likes = models.ManyToManyField(get_user_model(),
-                                   related_name="%(app_label)s_%(class)s_like_set",
-                                   related_query_name="%(app_label)s_%(class)s_likes")
 
     def __str__(self):
         return f"({self.author.username},{str(self.pub_time)},{self.text[:20]})"
 
 
-class Post(PublishedText):
+class Post(LikeableMixin, PublishedText):
     pass
 
 
-class Comment(PublishedText):
+class Comment(LikeableMixin, PublishedText):
     parent_content = models.ForeignKey(PublishedText, on_delete=models.CASCADE, related_name='comment_set')
